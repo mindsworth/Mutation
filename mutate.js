635425e96af13c76d2cb6dc90dc3c0c13cd80c36
Chigoziem Nwaiwu <chigodwin20@yahoo.com>
2017-01-29 13:02:02 +0100

    Update mutate.js
    It was simple but tricky
    
    Three variables are declared. The fElement, sElement variables stores the return of converting the provided array of index 0 and 1 respectively to lower case. While result stores Boolean true value if the index return a positive value greater than or equals 0.
    
    A for loop is then run to iterate through the sElement array element and check if itexist in fElement.

--- mutate.js
+++ mutate.js
@@ -1,5 +1,26 @@
+
 function mutation(arr) {
-  return arr;
+  
+  var fElement = arr[0].toLowerCase();
+  var sElement = arr[1].toLowerCase();
+  var result;
+  
+  for(var i = 0; i < sElement.length; i++) {
+    
+    result = fElement.indexOf(sElement[i]);
+    
+    if(result < 0) {
+      return false;
+    } else {
+      result = true;
+    }
+    
+    
+  }
+  
+  
+  
+  return result;
 }
 
 mutation(["hello", "hey"]);
